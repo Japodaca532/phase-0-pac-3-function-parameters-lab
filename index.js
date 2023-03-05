092bcc87974f6444f18d93256475bbff55b92e61
Jacqueline Apodaca <jacapodaca42@gmail.com>
2023-03-04 20:55:09 -0700

    file1.md

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function introduction(name) {
 function introductionWithLanguage(name, language) {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
-function intintroductionWithLanguageOptional(name, language = "JavaScript") {
+function intintroductionWithLanguageOptional(name, language =  "Hello, User!") {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
